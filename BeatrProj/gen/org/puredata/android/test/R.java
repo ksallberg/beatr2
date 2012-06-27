f9b8d90553cf327e51cf447c28c129d59a6ddffa
Kristian Sällberg <kristian@purestyle.se>
2012-06-27 19:10:08 +0200

    test

--- BeatrProj/gen/org/puredata/android/test/R.java
+++ BeatrProj/gen/org/puredata/android/test/R.java
@@ -21,7 +21,8 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int beatr=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
         public static final int about_item=0x7f0a0006;
