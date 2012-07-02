6aaaf8bb198d3ae97618dfc2bdd28788ce511b23
Kristian Sällberg <kristian@purestyle.se>
2012-07-02 16:58:00 +0200

    Added grounds for the Start view, this will wrap InstrumentMixerView among others

---
+++ BeatrProj/src/se/purestyle/beatr/view/instrumentmixer/StartView.java
@@ -0,0 +1,14 @@
+package se.purestyle.beatr.view.instrumentmixer;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+public class StartView extends LinearLayout {
+
+	public StartView(Context context) {
+		
+		super(context);
+		
+		
+	}
+}
