e31818688880ca22fe79661f7e8b7cb1145ab95c
Kristian Sällberg <kristian@purestyle.se>
2012-07-27 22:26:03 +0200

    Fixed bug that made the knob's status indicator flip out in resolutions other than 480x800

--- BeatrProj/src/se/purestyle/beatr/view/generic/KnobView.java
+++ BeatrProj/src/se/purestyle/beatr/view/generic/KnobView.java
@@ -29,12 +29,15 @@ public class KnobView extends View {
 		p.setColor( Color.parseColor( "#434343" ) );
 		p.setAntiAlias( true );
 		
-		canvas.drawCircle( getWidth() / 2, getHeight() / 2, getWidth() / 2, p ); //( 0, 0, getWidth(), getHeight(), p );
+		canvas.drawCircle( getWidth() / 2, getHeight() / 2, getWidth() / 2, p );
 		
 		Paint linePaint = new Paint();
 		linePaint.setColor( Color.parseColor( "#00ffe4" ) );
-		//linePaint.setAlpha( 200 );
 		linePaint.setAntiAlias( true );
+		
+		Paint redPaint = new Paint();
+		redPaint.setColor( Color.parseColor( "#ff0000" ) );
+		redPaint.setAlpha( 200 );
 		
 		canvas.save();
 		
@@ -42,11 +45,15 @@ public class KnobView extends View {
 		getLocationOnScreen( apa );
 		
 		Matrix m = new Matrix();
-		m.preTranslate( apa[0] * 1.5f + (getWidth() / 2) * 1.5f, apa[1] * 1.5f + (getHeight() / 2) * 1.5f );
-		m.preRotate( model.getCurrentAngle() + 45 );
+		//m.preTranslate( apa[0] + (getWidth() ), apa[1] + (getHeight() ) );
+		//m.preTranslate( apa[0] * 1.5f + (getWidth() / 2) * 1.5f, apa[1] * 1.5f + (getHeight() / 2) * 1.5f );
+		//m.preRotate( model.getCurrentAngle() + 45 );
 		
-		canvas.setMatrix( m );
-		canvas.drawCircle( 0, getHeight() / 2, getWidth() / 10, linePaint );
+		m.reset();
+		m.preTranslate( (getWidth() / 2 ), (getHeight() / 2 ) );
+		m.preRotate( model.getCurrentAngle() + 45 );
+		canvas.concat(m);
+		canvas.drawCircle( 0, getHeight() / 4, getWidth() / 10, linePaint );
 		
 		canvas.restore();
 	}
