64826a205c30872c31c74cf1901f68c3f757c76c
Kristian Sällberg <kristian@purestyle.se>
2012-08-05 15:27:10 +0200

    Added getters/setters and storage variables for all the settings currently in the drum

--- BeatrProj/src/se/purestyle/beatr/model/editors/DrumEditorModel.java
+++ BeatrProj/src/se/purestyle/beatr/model/editors/DrumEditorModel.java
@@ -15,22 +15,65 @@ public class DrumEditorModel extends AbstractEditorModel implements PropertyChan
 	
 	private final String pdInternalInstrumentName;
 	
-	private final String onOffControllerName = "onoff";
-	private int onoff = 0;
+	private final String onOffControllerName 			= "onoff";
+	private int onoff 									= 0;
 	
-	private final String volControllerName = "vol";
-	private final String pulseControllerName = "pulse";
+	private final String volControllerName 				= "vol";
+	private final String pulseControllerName 			= "pulse";
 	
+	//root
+	private final String rootControllerName 			= "root";
+	private 	  float root 							= 2.2f;
+	
+	//f01
+	private final String f01ControllerName 				= "f01";
+	private 	  float f01								= 2.2f;
+	
+	//f02
+	private final String f02ControllerName 				= "f02";
+	private 	  float f02								= 2.2f;
+	
+	//clip
+	private final String clipControllerName 			= "clip";
+	private 	  float clip							= 2.2f;
+	
+	//shape
+	private final String shapeControllerName 			= "shape";
+	private 	  float shape							= 2.2f;
+	
+	//decay
+	private final String decayControllerName 			= "decay";
+	private 	  float decay							= 2.2f;
+	
+	//mod
+	private final String modControllerName 				= "mod";
+	private 	  float mod								= 2.2f;
+
+	/**
+	 * Constructor, needs the name of this instrument.
+	 * 
+	 * @param pdInternalInstrumentName
+	 */
 	public DrumEditorModel( String pdInternalInstrumentName ) {
 		
 		this.pdInternalInstrumentName = pdInternalInstrumentName;
 		
 		//Prepare a new .pd file that has variable names unique to this model
 		Map<String, String> replacementMap = new HashMap<String, String>();
-		replacementMap.put( volControllerName, pdInternalInstrumentName + volControllerName );
-		replacementMap.put( onOffControllerName, pdInternalInstrumentName + onOffControllerName );
-		replacementMap.put( pulseControllerName, pdInternalInstrumentName + pulseControllerName );
+		replacementMap.put( volControllerName, pdInternalInstrumentName 	+ volControllerName );
+		replacementMap.put( onOffControllerName, pdInternalInstrumentName 	+ onOffControllerName );
+		replacementMap.put( pulseControllerName, pdInternalInstrumentName 	+ pulseControllerName );
 		
+		//Drum settings, make them specific for this instrument
+		replacementMap.put( rootControllerName, pdInternalInstrumentName 	+ rootControllerName );
+		replacementMap.put( f01ControllerName, pdInternalInstrumentName 	+ f01ControllerName );
+		replacementMap.put( f02ControllerName, pdInternalInstrumentName 	+ f02ControllerName );
+		replacementMap.put( clipControllerName, pdInternalInstrumentName 	+ clipControllerName );
+		replacementMap.put( shapeControllerName, pdInternalInstrumentName 	+ shapeControllerName );
+		replacementMap.put( decayControllerName, pdInternalInstrumentName 	+ decayControllerName );
+		replacementMap.put( modControllerName, pdInternalInstrumentName 	+ modControllerName );
+		
+		//Load the drum file!
 		File newDrumFile = FileModifier.createIndividualizedFile( replacementMap, "pdfiles/drum.pd" );
 		
 		FileModifier.traceFile( newDrumFile );
@@ -73,16 +116,86 @@ public class DrumEditorModel extends AbstractEditorModel implements PropertyChan
 		
 		this.onoff = onoff;
 		
-		PdConnector.sendToPd( pdInternalInstrumentName + onOffControllerName, this.onoff );
+		PdConnector.sendToPd( pdInternalInstrumentName + onOffControllerName, this.onoff ); //This. just to avoid unused warning
 	}
 
+	/*
+	 * Below here, a lot of setters and getters live!
+	 * 
+	 */
+	
+//	Root_________________________________________________
+	public void setRoot( float root ) {
+		
+		this.root = root;
+		PdConnector.sendToPd( pdInternalInstrumentName + rootControllerName, root );
+	}
+	
+	public float getRoot() { return root; }
+
+//	f01_________________________________________________
+	public void setF01( float f01 ) {
+		
+		this.f01 = f01;
+		PdConnector.sendToPd( pdInternalInstrumentName + f01ControllerName, f01 );
+	}
+	
+	public float getF01() { return f01; }
+
+//	f02_________________________________________________
+	public void setF02( float f02 ) {
+		
+		this.f02 = f02;
+		PdConnector.sendToPd( pdInternalInstrumentName + f02ControllerName, f02 );
+	}
+	
+	public float getF02() { return f02; }
+
+//	clip_________________________________________________
+	public void setClip( float clip ) {
+		
+		this.clip = clip;
+		PdConnector.sendToPd( pdInternalInstrumentName + clipControllerName, clip );
+	}
+	
+	public float getClip() { return clip; }
+
+//	shape_________________________________________________
+	public void setShape( float shape ) {
+		
+		this.shape = shape;
+		PdConnector.sendToPd( pdInternalInstrumentName + shapeControllerName, shape );
+	}
+	
+	public float getShape() { return shape; }
+	
+//	decay_________________________________________________
+	public void setDecay( float decay ) {
+		
+		this.decay = decay;
+		PdConnector.sendToPd( pdInternalInstrumentName + decayControllerName, decay );
+	}
+	
+	public float getDecay() { return decay; }
+
+//	mod_________________________________________________
+	public void setMod( float mod ) {
+		
+		this.mod = mod;
+		PdConnector.sendToPd( pdInternalInstrumentName + modControllerName, mod );
+	}
+	
+	public float getMod() { return mod; }
+	
 	@Override
 	public void propertyChange( PropertyChangeEvent event ) {
 		
 		//Tick event dispatched by the metronome
 		if( event.getPropertyName().equals( MetronomePlayer.TICK ) ) {
 			
+			//Play the drum!
 			PdConnector.sendToPd( pdInternalInstrumentName + pulseControllerName, 1.0f );
+			//Make the drum go quiet again
 			PdConnector.sendToPd( pdInternalInstrumentName + pulseControllerName, 0.0f );
 		}
 	}
