34d1920898ee5564ec3108106aee8dd85f900104
Kristian Sällberg <kristian@purestyle.se>
2012-08-07 15:45:28 +0200

    Removed some old png files

--- BeatrProj/gen/se/purestyle/beatr/R.java
+++ BeatrProj/gen/se/purestyle/beatr/R.java
@@ -22,25 +22,15 @@ public final class R {
     }
     public static final class drawable {
         public static final int addinstrumentbutton=0x7f020000;
-        public static final int beatr=0x7f020001;
-        public static final int bg=0x7f020002;
-        public static final int editinstrument=0x7f020003;
-        public static final int icon=0x7f020004;
-        public static final int logo=0x7f020005;
-        public static final int mastervolumebg=0x7f020006;
-        public static final int mastervolumefg=0x7f020007;
-        public static final int next=0x7f020008;
-        public static final int prev=0x7f020009;
-        public static final int rewindinstrument=0x7f02000a;
-        public static final int sample_0=0x7f02000b;
-        public static final int sample_1=0x7f02000c;
-        public static final int sample_2=0x7f02000d;
-        public static final int sample_3=0x7f02000e;
-        public static final int sample_4=0x7f02000f;
-        public static final int sample_5=0x7f020010;
-        public static final int sample_6=0x7f020011;
-        public static final int sample_7=0x7f020012;
-        public static final int volumebaroverlay=0x7f020013;
+        public static final int editinstrument=0x7f020001;
+        public static final int icon=0x7f020002;
+        public static final int logo=0x7f020003;
+        public static final int mastervolumebg=0x7f020004;
+        public static final int mastervolumefg=0x7f020005;
+        public static final int next=0x7f020006;
+        public static final int prev=0x7f020007;
+        public static final int rewindinstrument=0x7f020008;
+        public static final int volumebaroverlay=0x7f020009;
     }
     public static final class id {
         public static final int about_item=0x7f0a0001;
@@ -57,8 +47,7 @@ public final class R {
         public static final int extra_ext=0x7f050001;
         public static final int extra_ext_v7a=0x7f050002;
         public static final int metronome=0x7f050003;
-        public static final int rewindinstrument=0x7f050004;
-        public static final int silence=0x7f050005;
+        public static final int silence=0x7f050004;
     }
     public static final class string {
         public static final int about_msg=0x7f070015;
